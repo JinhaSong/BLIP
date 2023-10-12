55ee7acaa2784809c5ed6b374e0d0591f6dc4b77
JinhaSong <jinhasong@sogang.ac.kr>
2023-10-13 00:12:58 +0900

    Update blip.py - transformer==4.32 기준으로 업데이트(diffusers version dependency)

--- blip.py
+++ blip.py
@@ -3,6 +3,7 @@ import torch
 from PIL import Image
 from torchvision import transforms
 from torchvision.transforms.functional import InterpolationMode
+from transformers import BlipProcessor, BlipForConditionalGeneration
 
 sys.path.insert(0, '/workspace/module/image2text/blip')
 from models.blip import blip_decoder
@@ -11,23 +12,17 @@ class BLIP:
     def __init__(self):
         self.image_size = 384
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+        self.model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base").to(self.device)
+
         self.model = blip_decoder(pretrained="/workspace/module/weights/model_base_capfilt_large.pth", image_size=self.image_size, vit='base', med_config="/workspace/module/image2text/blip/configs/med_config.json")
         self.model.eval()
         self.model = self.model.to(self.device)
 
-    def load_image(self, image_path):
+
+    def generate_caption(self, image_path, text="a photography of"):
         raw_image = Image.open(image_path).convert('RGB')
 
-        transform = transforms.Compose([
-            transforms.Resize((self.image_size,self.image_size),interpolation=InterpolationMode.BICUBIC),
-            transforms.ToTensor(),
-            transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
-            ])
-        image = transform(raw_image).unsqueeze(0).to(self.device)
-        return image
-
-    def generate_caption(self, image_path):
-        image = self.load_image(image_path)
-        with torch.no_grad():
-            captions = self.model.generate(image, sample=False, num_beams=3, max_length=20, min_length=5)
-            return captions
+        inputs = self.processor(raw_image, text, return_tensors="pt").to("cuda")
+        out = self.model.generate(**inputs)
+        return self.processor.decode(out[0], skip_special_tokens=True)
